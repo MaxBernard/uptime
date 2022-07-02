1efa9d719e8814178545922fa0eb188976ffdf8f
Max Bernard <Max.Bernard328@gmail.com>
2022-07-02 11:25:23 -0400

    Initial commit, Form & Table working

---
+++ app/Filament/Resources/SiteResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SiteResource\Pages;
+use App\Filament\Resources\SiteResource\RelationManagers;
+use App\Models\Site;
+use Filament\Forms;
+use Filament\Forms\FormsComponent;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SiteResource extends Resource
+{
+  protected static ?string $model = Site::class;
+
+  protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+  public static function form(Form $form): Form
+  {
+    return $form
+      ->columns(1)
+      ->schema([
+        Forms\Components\TextInput::make('name')->required(),
+        Forms\Components\Repeater::make('urls')
+          ->label('URLs')
+          ->schema([
+            Forms\Components\TextInput::make('name')->required(),
+            Forms\Components\TextInput::make('url')->label('URL')->url()->required(),
+          ])
+          ->createItemButtonLabel('Add URL')
+          ->nullable(),
+          ]);
+      
+  }
+
+  public static function table(Table $table): Table
+  {
+    return $table
+      ->columns([
+        Tables\Columns\TextColumn::make('name'),
+        Tables\Columns\TextColumn::make('urls')
+          ->label('No. of URLs')
+          ->getStateUsing(function(Site $record) {
+            return count($record->urls);
+          })
+      ])
+      ->filters([
+        //
+      ])
+      ->actions([
+        Tables\Actions\EditAction::make(),
+      ])
+      ->bulkActions([
+        Tables\Actions\DeleteBulkAction::make(),
+      ]);
+  }
+    
+  public static function getRelations(): array
+  {
+    return [
+      //
+    ];
+  }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSites::route('/'),
+            'create' => Pages\CreateSite::route('/create'),
+            'edit' => Pages\EditSite::route('/{record}/edit'),
+        ];
+    }    
+}
